7f395a46a4fdfd3052a18960975603c925475a10
fradin.emma <fradin.emma@appirits.com>
2024-03-17 10:29:48 +0900

    member plugin

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -3,6 +3,8 @@
 function university_post_types() {
   // campus post type
   register_post_type('campus', array(
+    'capability_type' => 'campus',
+    'map_meta_cap' => true,
     'supports' => array('title', 'editor', 'excerpt'),
     'rewrite' => array(
       'slug' => 'campuses'
@@ -23,6 +25,8 @@ function university_post_types() {
 
   // event post type
   register_post_type('event', array(
+    'capability_type' => 'event',
+    'map_meta_cap' => true,
     'supports' => array('title', 'editor', 'excerpt'),
     'rewrite' => array(
       'slug' => 'events'
